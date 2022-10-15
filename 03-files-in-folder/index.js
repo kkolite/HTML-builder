ec1115b53629c56be4bd1469c19ed801d2539ec6
kkolite <litavoregor@mail.ru>
2022-10-15 22:37:02 +0300

    feat: solve copy directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { stdout } = process;
 
 stdout.write('Hi! Get info about files in folder!\n');
-let names = [];
 const src = path.join(__dirname, 'secret-folder');
 fs.readdir(src, {withFileTypes: true},   (err, files) => {
     if (err)
@@ -17,7 +16,6 @@ fs.readdir(src, {withFileTypes: true},   (err, files) => {
                   console.log(error);
                 }
                console.log(file.name, ' - ', path.extname(file.name), ' - ', stats.size)
-               //console.log(stats)
             })
         }
       })
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcCopy = path.join(__dirname, 'files-copy');
+const srcOrigin = path.join(__dirname, 'files');
+fs.mkdir(srcCopy, {recursive: true}, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+});
+fs.readdir(srcOrigin, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.copyFile(path.join(srcOrigin, file), path.join(srcCopy, file), (err) => {
+            if (err) throw err;
+            console.log(`${file} was copied`);
+          })
+      })
+    }
+});
