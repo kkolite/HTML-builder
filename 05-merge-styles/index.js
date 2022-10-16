132a750cc5a0bb088cc4aff5bcc55f4e49e4a6eb
kkolite <litavoregor@mail.ru>
2022-10-16 15:31:05 +0300

    feat: solve merge styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcOrigin = path.join(__dirname, 'styles');
+const src = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.writeFile(src,'', err => {if (err) throw err});
+const output = fs.createWriteStream(src);
+
+fs.readdir(srcOrigin, {withFileTypes: true},   (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        if (!file.isDirectory() && path.extname(file.name).match('css')) {
+            //const stream = fs.createReadStream(path.join(srcOrigin, file.name));
+            //stream.on('data', chunk => /*data.push(chunk)*/ console.log(chunk));
+            let url = path.join(srcOrigin, file.name)
+            fs.readFile(url, 'utf8', function(err, data){
+                output.write(data);
+                console.log(data);
+            })
+        }
+      })
+    }
+});
